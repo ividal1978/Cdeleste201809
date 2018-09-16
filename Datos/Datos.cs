8c290ce56b088959fec34f5d266a3de7236bf5e4
ividal1978 <bluespiritst@gmail.com>
2018-09-16 04:06:45 -0300

    se genera la primer conexion se aplica Nlog sin configurar

--- Datos/Datos.cs
+++ Datos/Datos.cs
@@ -11,15 +11,40 @@ namespace Datos
 {
     public partial class Datos
     {
-        public string StringConnection = "Server=localhost;Database=cdeleste_DB;User ID=cdeleste_rpt ;Password=costa10";
+        public string StringConnection = System.Configuration.ConfigurationManager.ConnectionStrings["CdelesteDBConnectionString"].ConnectionString;
+        //public string StringConnection = "Server=localhost;Database=cdeleste_DB;User ID=cdeleste_rpt ;Password=costa10";
         public Usuarios GetUsuario(string Usuario, string Password)
         {
-            Usuarios oUsuario = new Usuarios();
-            //        System.Configuration.ConfigurationManager.
-            //ConnectionStrings["connectionStringName"].ConnectionString;
+            try
+            {
+                Usuarios oUsuario = new Usuarios();
+                MySqlConnection oConeccion = new MySqlConnection(StringConnection);
+                string query = "SELECT Apellido,Nombre, Usuario,Rol ,PASSWORD FROM Usuarios "
+                + " WHERE Usuario = '" + Usuario + "' AND PASSWORD = '" + Password + "'";
 
-            string StringConnection = System.Configuration.ConfigurationManager.ConnectionString["CdelesteDBConnectionString"].ConnectionString;
+                oConeccion.Open();
 
+                MySqlCommand myCommand = new MySqlCommand(query, oConeccion);
+
+                MySqlDataAdapter myDA = new MySqlDataAdapter(myCommand);
+
+                DataSet myDS = new DataSet();
+                myDA.Fill(myDS, "Usuario");
+
+                if (myDS.Tables["Usuario"].Rows.Count > 0)
+                {
+                    oUsuario.Apellido = myDS.Tables["Usuario"].Rows[0].ItemArray[0].ToString();
+                    oUsuario.Nombre = myDS.Tables["Usuario"].Rows[0].ItemArray[1].ToString();
+                    oUsuario.Usuario = myDS.Tables["Usuario"].Rows[0].ItemArray[2].ToString();
+                    oUsuario.Rol = myDS.Tables["Usuario"].Rows[0].ItemArray[3].ToString();
+                    oUsuario.Password = myDS.Tables["Usuario"].Rows[0].ItemArray[4].ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+              
+            }
             //buscar en base
             return oUsuario;
         }
