463c3aac75d62b6e297dea259b0720f93b9b6274
ividal1978 <bluespiritst@gmail.com>
2018-09-24 03:40:44 -0300

    Login redirige a menu

---
+++ WebApplication1/Menu.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Cdeleste.Master" AutoEventWireup="true" CodeBehind="Menu.aspx.cs" Inherits="WebApplication1.Menu" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+          <script src="Scripts/jquery-3.0.0.min.js"></script>
+        <script src="Scripts/bootstrap.min.js"></script>
+            <link href="Scripts/Cdeleste.css" rel="stylesheet" />
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+  
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+   <div class="container-fluid"> 
+       <div class="row">
+            <br />
+
+       Beinvenidos al la intranet
+      <div style="height: 382px; margin-top: 75px;">
+    <form id="menu" runat="server">
+    <asp:Button ID="btnInquilinos" runat="server" Text="Inquilinos"  BackColor="White"/>
+        </form>
+    </div>
+  </div>
+   </div>
+    
+ 
+</asp:Content>
---
+++ WebApplication1/Menu.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Menu : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login()
+        {
+
+            string usuario = "";//usuarioInput.Value.ToString(); //Request.QueryString["usuarioInput"];
+            string password = "";//passwordInput.Value.ToString(); //Request.QueryString["passwordInput"];
+            Negocio.Negocio oNegocio = new Negocio.Negocio();
+            if (usuario.Length > 0)
+            {
+                var Acceso = oNegocio.GetUsuario(usuario, password);
+                if (Acceso != null)
+                {
+                    Session["usuario"] = Acceso.Usuario;
+                    Session["fInicioUsuario"] = DateTime.Now;
+                    Response.Redirect("Menu.aspx");
+
+                }
+            }
+        }
+    }
+}
