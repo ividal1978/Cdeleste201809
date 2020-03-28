5dca907a83632e9565af02c09f8a55899e8a2244
ividal1978 <bluespiritst@gmail.com>
2020-03-28 01:54:36 -0300

    rutina de guardado de imagen

--- WebApplication1/Galeria_ABM.aspx.cs
+++ WebApplication1/Galeria_ABM.aspx.cs
@@ -143,6 +143,55 @@ namespace WebApplication1
             }
             return counter;
         }
+
+        void SaveFile(HttpPostedFile file)
+        {
+            // Specify the path to save the uploaded file to.
+            string savePath = "~/Imagenes/Galeria/";//"c:\\temp\\uploads\\";
+
+            // Get the name of the file to upload.
+            string fileName = fUpload.FileName;
+
+            // Create the path and file name to check for duplicates.
+            string pathToCheck = savePath + fileName;
+
+            // Create a temporary file name to use for checking duplicates.
+            string tempfileName = "";
+
+            // Check to see if a file already exists with the
+            // same name as the file to upload.        
+            if (System.IO.File.Exists(pathToCheck))
+            {
+                int counter = 2;
+                while (System.IO.File.Exists(pathToCheck))
+                {
+                    // if a file with this name already exists,
+                    // prefix the filename with a number.
+                    tempfileName = counter.ToString() + fileName;
+                    pathToCheck = savePath + tempfileName;
+                    counter++;
+                }
+
+                fileName = tempfileName;
+
+                // Notify the user that the file name was changed.
+               //-- UploadStatusLabel.Text = "A file with the same name already exists." +
+               //--     "<br />Your file was saved as " + fileName;
+            }
+            else
+            {
+                // Notify the user that the file was saved successfully.
+                LbError.Text = "Your file was uploaded successfully.";
+            }
+
+            // Append the name of the file to upload to the path.
+            savePath += fileName;
+
+            // Call the SaveAs method to save the uploaded
+            // file to the specified directory.
+            fUpload.SaveAs(savePath);
+
+        }
     }
 
 }
